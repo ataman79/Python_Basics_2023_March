c2df6d0a4b0b5a866d7747623b93b1f215750f14
ataman79 <s_minev@abv.bg>
2023-03-13 23:37:58 +0200

    First steps in coding - Lab

---
+++ first_steps_in_coding/pet_shop.py
@@ -0,0 +1,9 @@
+dog_food_count = int(input())
+cat_food_count = int(input())
+'''
+dog_pack_price = dog_food_count * 2.5
+cat_pack_price = cat_food_count * 4
+sum_food = dog_pack_price + cat_pack_price
+'''
+sum_food = dog_food_count * 2.5 + cat_food_count * 4
+print(f"{sum_food} lv.")
---
+++ first_steps_in_coding/yard_greening.py
@@ -0,0 +1,6 @@
+square_meters = float(input())
+total_price = square_meters * 7.61
+discount_price = total_price * 0.18
+final_price = total_price - discount_price
+print(f"The final price is: {final_price} lv.")
+print(f"The discount is: {discount_price} lv.")
