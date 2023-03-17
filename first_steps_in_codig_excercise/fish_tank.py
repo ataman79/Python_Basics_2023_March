8fee13dd238734c7b892bc157cdf3f260ef4e2ec
SAMEX\s_minev <s_minev@abv.bg>
2023-03-17 16:43:26 +0200

    exercises are uploaded

--- first_steps_in_codig_excercise/fish_tank.py
+++ first_steps_in_codig_excercise/fish_tank.py
@@ -12,3 +12,25 @@ water_needed = volume_l * water_percent
 print(water_needed)
 
 
+
+
+# # Read in the dimensions and percentage from the console
+# length = int(input())
+# width = int(input())
+# height = int(input())
+# percentage = float(input())
+#
+# # Calculate the volume of the aquarium in cubic centimeters
+# volume_cm3 = length * width * height
+#
+# # Calculate the volume of the non-water components in cubic centimeters
+# non_water_volume_cm3 = percentage/100 * volume_cm3
+#
+# # Calculate the volume of water in cubic centimeters
+# water_volume_cm3 = volume_cm3 - non_water_volume_cm3
+#
+# # Convert the volume of water to liters
+# water_volume_l = water_volume_cm3/1000
+#
+# # Output the result to the console
+# print(water_volume_l)
