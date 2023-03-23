8497f5b6ca8991021ea7b6cfe58760e1fea58e42
ataman79 <s_minev@abv.bg>
2023-03-23 20:17:17 +0200

    conditional statement exercise

--- conditionl_statements_excercise/bonus_score.py
+++ conditionl_statements_excercise/bonus_score.py
@@ -3,18 +3,18 @@ bonus = 0
 
 if number <= 100:
     bonus += 5
-
-elif number > 100:
-    bonus = number * 0.20
-
 elif number > 1000:
-    bonus  = number * 0.10
+    bonus = number * 0.10
 
+else:
+   bonus = number * 0.20
 
 if number % 2 == 0:
     bonus += 1
 
-
-
+elif number % 10 == 5:
+    bonus += 2
 
 print(bonus)
+total = number + bonus
+print(total)
