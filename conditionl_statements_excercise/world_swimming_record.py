3300384be28e4e1210afa0ebc39fe1ccf75eecee
ataman79 <s_minev@abv.bg>
2023-04-03 20:44:05 +0300

    conditional statement exercise

--- conditionl_statements_excercise/world_swimming_record.py
+++ conditionl_statements_excercise/world_swimming_record.py
@@ -8,10 +8,10 @@ time_for_distance = distance_meters * time_sec_meter
 slowing_down = floor(distance_meters / 15) * 12.5
 total_time = time_for_distance + slowing_down
 
-diff = total_time - record_sec
+diff = abs(total_time - record_sec)
 
-if record_sec < total_time:
-    print(f"No, he failed! He was {diff} seconds slower.")
+if total_time < record_sec:
+    print(f"Yes, he succeeded! The new world record is {total_time:.2f} seconds.")
 
-elif record_sec > total_time:
-    print(f"Yes, he succeeded! The new world record is {diff} seconds.")
+else:
+    print(f"No, he failed! He was {diff:.2f} seconds slower.")
