45cbb473252a60a3bd7898dfe4a0fb5222181e2d
SAMEX\s_minev <s_minev@abv.bg>
2023-04-04 16:14:50 +0300

    Conditional-Statements Advanced - Lab are added

---
+++ conditional_statements_advanced_lab/cinema_tickets.py
@@ -0,0 +1,13 @@
+weekday = input()
+
+if weekday == "Monday" or weekday == "Tuesday":
+    print("12")
+
+elif weekday == "Wednesday" or weekday == "Thursday":
+    print("14")
+
+elif weekday == "Saturday" or weekday == "Sunday":
+    print("16")
+
+else:
+    print("12")
---
+++ conditional_statements_advanced_lab/working_hours.py
@@ -0,0 +1,13 @@
+current_time = int(input())
+week_day = input()
+
+if week_day == "Monday" or week_day == "Tuesday" or week_day == "Wednesday" or week_day == "Thursday" or \
+        week_day == "Friday" or week_day == "Saturday":
+    if 10 <= current_time <= 18:
+        print("open")
+
+    else:
+        print("closed")
+
+elif week_day == "Sunday":
+    print("closed")
